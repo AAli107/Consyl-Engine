3ce93621deef254c213650713a1ea6738a0ced2d
Ali107 <Alialawadhi2014bh@gmail.com>
2020-09-18 02:07:34 +0300

    Added Support for Colors

--- EngineContents/gfx.cs
+++ EngineContents/gfx.cs
@@ -18,28 +18,34 @@ namespace Consyl_Engine.EngineContents
             'j','r','x','n','u','v','c','z','X','Y','U','J','C','L','Q','O','Z','m','w','q','p','d','b','k','h','a','o','#','M','W','&','%','B','@','$' };
 
         static char[] textImage = new char[drawWidth * drawHeight]; // Stores the Data of the ASCII pixels
-
+        static ConsoleColor[] ColorImage = new ConsoleColor[drawWidth * drawHeight]; // Stores the Color Data of the ASCII pixels
+        
         public static void DrawASCII()
         {
             // Draws ASCII Render on screen
-            string renderedImage = "";
+            //string renderedImage = "";
             for (int y = 0; y < drawHeight; y++)
             {
                 // Converts 1D array to 2D array
                 List<char> xArray = new List<char>(drawWidth);
+                List<ConsoleColor> cArray = new List<ConsoleColor>(drawWidth);
                 for (int x = 0; x < drawWidth; x++)
                 {
                     xArray.Add(textImage[drawWidth * y + x]);
+                    cArray.Add(ColorImage[drawWidth * y + x]);
                 }
 
-                string renderLine = "";
+                //string renderLine = "";
                 for (int i = 0; i < xArray.Count; i++) // stores a line of the screen
                 {
-                    renderLine = renderLine + xArray[i] + " ";
+                    Console.ForegroundColor = cArray[i];
+                    //renderLine = renderLine + xArray[i] + " ";
+                    Console.Write(xArray[i] + " ");
                 }
-                renderedImage = renderedImage + renderLine + "\n";
+                Console.WriteLine("");
+                //renderedImage = renderedImage + renderLine + "\n";
             }
-            Console.WriteLine(renderedImage);
+            //Console.WriteLine(renderedImage);
         }
 
         public static void ClearScreen()
@@ -51,40 +57,42 @@ namespace Consyl_Engine.EngineContents
             // Clears the Image data from textImage
             for (int i = 0; i < textImage.Length; i++)
             {
-                textImage[i] = shadeCharArray[0];
+                textImage[i] = shadeCharArray[1];
+                ColorImage[i] = ConsoleColor.White;
             }
         }
 
-        public static void DrawPixel(int x, int y, char pixelLook) // Draws ASCII pixel on screen
+        public static void DrawPixel(int x, int y, char pixelLook, ConsoleColor color) // Draws ASCII pixel on screen
         {
             if (x >= 0 && x < drawWidth && y >= 0 && y < drawHeight)
             {
                 textImage[drawWidth * y + x] = pixelLook;
+                ColorImage[drawWidth * y + x] = color;
             }
         }
 
-        public static void DrawRectangle(int x, int y, int width, int height, char pixelLook) // Draws an ASCII rectangle on screen
+        public static void DrawRectangle(int x, int y, int width, int height, char pixelLook, ConsoleColor color) // Draws an ASCII rectangle on screen
         {
             for (int y0 = y; y0 < height + y; y0++)
             {
                 for (int x0 = x; x0 < width + x; x0++)
                 {
-                    DrawPixel(x0, y0, pixelLook);
+                    DrawPixel(x0, y0, pixelLook, color);
                 }
             }
         }
-        public static void DrawText(int x, int y, string text) // Draws text in a position on screen
+        public static void DrawText(int x, int y, string text, ConsoleColor color) // Draws text in a position on screen
         {
             char[] charArray = new char[text.Length];
             charArray = text.ToCharArray();
 
             for (int i = 0; i < text.Length; i++)
             {
-                DrawPixel(x + i, y, charArray[i]);
+                DrawPixel(x + i, y, charArray[i], color);
             }
         }
 
-        public static void DrawLine(int x0, int y0, int x1, int y1, char pixelLook) // Draw lines between two points
+        public static void DrawLine(int x0, int y0, int x1, int y1, char pixelLook, ConsoleColor color) // Draw lines between two points
         {
             int dx = Math.Abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
             int dy = Math.Abs(y1 - y0), sy = y0 < y1 ? 1 : -1;
@@ -92,7 +100,7 @@ namespace Consyl_Engine.EngineContents
 
             for (; ; )
             {
-                DrawPixel(x0, y0, pixelLook);
+                DrawPixel(x0, y0, pixelLook, color);
 
                 if (x0 == x1 && y0 == y1) 
                     break;
@@ -112,7 +120,7 @@ namespace Consyl_Engine.EngineContents
             }
         }
 
-        public static void DrawFilledCircle(int centerX, int centerY, float radius, char pixelLook) // Draws a Filled Circle
+        public static void DrawFilledCircle(int centerX, int centerY, float radius, char pixelLook, ConsoleColor color) // Draws a Filled Circle
         {
             for (double i = 0.0; i < 360; i += 0.1)
             {
@@ -122,12 +130,12 @@ namespace Consyl_Engine.EngineContents
                     int x = (int)(r * System.Math.Cos(angle));
                     int y = (int)(r * System.Math.Sin(angle));
 
-                    DrawPixel(x + centerX, y + centerY, pixelLook);
+                    DrawPixel(x + centerX, y + centerY, pixelLook, color);
                 }
             }
         }
 
-        public static void DrawCircle(int centerX, int centerY, float radius, char pixelLook) // Draws a Circle
+        public static void DrawCircle(int centerX, int centerY, float radius, char pixelLook, ConsoleColor color) // Draws a Circle
         {
             for (double i = 0.0; i < 360; i += 0.1)
             {
@@ -135,17 +143,17 @@ namespace Consyl_Engine.EngineContents
                     int x = (int)(radius * System.Math.Cos(angle));
                     int y = (int)(radius * System.Math.Sin(angle));
 
-                    DrawPixel(x + centerX, y + centerY, pixelLook);
+                    DrawPixel(x + centerX, y + centerY, pixelLook, color);
             }
         }
 
-        public static void DrawTri(int x, int y, int size, char pixelLook) // Draws a right angle triangle
+        public static void DrawTri(int x, int y, int size, char pixelLook, ConsoleColor color) // Draws a right angle triangle
         {
             for (int i = x; i < size; i++)
             {
                 for (int j = 0; j < i; j++)
                 {
-                    DrawPixel(x + j, y + i, pixelLook);
+                    DrawPixel(x + j, y + i, pixelLook, color);
                 }
             }
         }
