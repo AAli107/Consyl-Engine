4fcb7c4f816ba5214d8596ed6b73b2306a7bb481
Ali107 <Alialawadhi2014bh@gmail.com>
2020-09-18 17:15:05 +0300

    Added a utilities file which will stores useful functions

---
+++ EngineContents/Utilities.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Consyl_Engine.EngineContents
+{
+    class Utilities
+    {
+        public class Rand
+        {
+            static Random rng = new Random(); // The Random class inside a variable
+
+            public static int RangeInt(int min, int max) // Returns a random int in range (inclusive)
+            {
+                return rng.Next(min, max + 1);
+            }
+
+            public static float RangeFloat(float min, float max) // Returns a random float in range
+            {
+                return (float)rng.NextDouble() * (max - min) + min;
+            }
+
+            public static bool RandBool() // Returns a random bool, true or false
+            {
+                int n = rng.Next(0, 2);
+                if (n == 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public static int RandInt(int max) // Returns a random int between 0 to max
+            {
+                return rng.Next(max + 1);
+            }
+        } // class that stores functions that returns random values
+    }
+}
