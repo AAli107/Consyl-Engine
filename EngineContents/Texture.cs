80e7457c89d5d0a886ea100adb11c08656c39a0b
Ali107 <Alialawadhi2014bh@gmail.com>
2020-11-17 19:54:30 +0300

    Added comments for Texture.cs to have it easier to understand how it works.

--- EngineContents/Texture.cs
+++ EngineContents/Texture.cs
@@ -19,14 +19,20 @@ namespace Consyl_Engine.EngineContents
 
         public void DrawImage(int x, int y) // Will draw the loaded texture file 
         {
-            Bitmap img = new Bitmap(fileName);
+            Bitmap img = new Bitmap(fileName); // loads the image in a variable
+
+            // Loops between pixels
             for (int i = 0; i < img.Width; i++)
             {
                 for (int j = 0; j < img.Height; j++)
                 {
-                    Color pixel = img.GetPixel(i, j);
-                    float AvgColor = (pixel.R + pixel.G + pixel.B) / 3;
-                    int shade = (int)(AvgColor / 4.047619047619047619047619047619f);
+                    Color pixel = img.GetPixel(i, j); // saves the color value of the current pixel in a variable
+                    
+                    float AvgColor = (pixel.R + pixel.G + pixel.B) / 3; // Takes all the color data Red, green and blue to be a single average number
+
+                    int shade = (int)(AvgColor / 4.047619047619047619047619047619f); // This weird number is used to squeeze the average color, which could range between 0-255 into a range between 0-62
+                    
+                    // Locks the shade value to be between 0-62
                     if (shade < 0)
                     {
                         shade = 0;
@@ -35,7 +41,8 @@ namespace Consyl_Engine.EngineContents
                     {
                         shade = 62;
                     }
-                    gfx.DrawPixel(i + x, j + y, gfx.shadeCharArray[shade]);
+
+                    gfx.DrawPixel(i + x, j + y, gfx.shadeCharArray[shade]); // Draws the image
                 }
             }
         }
