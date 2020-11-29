d22ba38b32d0cfee5af53ab41cce1619b3144666
Ali107 <Alialawadhi2014bh@gmail.com>
2020-11-29 15:33:44 +0300

    Documentation Progress 5

--- GameCode.cs
+++ GameCode.cs
@@ -9,7 +9,8 @@ namespace Consyl_Engine
     {
          // Insert Variables here! \\
         // \/\/\/\/\/\/\/\/\/\/\/\/ \\
-
+        static Audio music = new Audio("Bruh Sound Effect #2.mp3");
+        static bool allowMusic = true;
         // /\/\/\/\/\/\/\/\/\/\/\/\ \\
 
         public static void OnGameStart() // Gets Executed when game starts running/when the game begins
@@ -19,7 +20,11 @@ namespace Consyl_Engine
 
         public static void OnGameUpdate() // Gets Executed every frame as long as the game is running
         {
-
+            if (Keyboard.IsKeyPressed(Key.S) & allowMusic)
+            {
+                music.PlaySound();
+                allowMusic = false;
+            }
         }
 
         public static void OnGameEnd() // Gets Executed when Engine.gameRunning = false which is basically when the game ends
