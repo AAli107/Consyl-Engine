c1054b0e66c3b43c6be050c920a570358fea2ddd
Ali107 <Alialawadhi2014bh@gmail.com>
2021-01-22 14:02:06 +0300

    Removed test code.

--- GameCode.cs
+++ GameCode.cs
@@ -7,10 +7,9 @@ namespace Consyl_Engine
 {
     class GameCode // Here is where you make most of your game's code!
     {
-        // Insert Variables here! \\
+         // Insert Variables here! \\
         // \/\/\/\/\/\/\/\/\/\/\/\/ \\
-        static bool rand = Utilities.Rand.RandBool();
-        static bool canPress = true;
+
         // /\/\/\/\/\/\/\/\/\/\/\/\ \\
 
         public static void OnGameStart() // Gets Executed when game starts running/when the game begins
@@ -20,30 +19,12 @@ namespace Consyl_Engine
 
         public static void OnGameUpdate() // Gets Executed every frame as long as the game is running
         {
-            if (Keyboard.IsKeyPressed(Key.Space))
-            {
-                if (canPress)
-                {
-                    rand = Utilities.Rand.RandBool();
-                    canPress = false;
-                }
-            }
-            else
-            {
-                canPress = true;
-            }
+            
         }
 
         public static void OnGraphicsUpdate() // Will be used to draw graphics related items per frame
         {
-            if (rand)
-            {
-                gfx.GameUI.DrawText(10, 10, "true");
-            }
-            else
-            {
-                gfx.GameUI.DrawText(10, 10, "false");
-            }
+            
         }
 
         public static void OnGameEnd() // Gets Executed when Engine.gameRunning = false which is basically when the game ends
