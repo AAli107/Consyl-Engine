8fe463293306615a83282805942f407d2fb71b68
Ali107 <Alialawadhi2014bh@gmail.com>
2021-07-30 20:36:25 +0300

    Updated the commenting in Utilities.cs so that they are summaries instead of flat comments.

--- EngineContents/Utilities.cs
+++ EngineContents/Utilities.cs
@@ -5,21 +5,40 @@ namespace Consyl_Engine.EngineContents
 {
     class Utilities
     {
+        /// <summary>
+        /// class that stores functions that returns rng-related values
+        /// </summary>
         public class Rand
         {
             static readonly Random rng = new Random(); // Variable that stores the Random Class
 
-            public static int RangeInt(int min, int max) // Returns a random int in range (inclusive)
+            /// <summary>
+            /// Returns a random int in range (inclusive)
+            /// </summary>
+            /// <param name="min"></param>
+            /// <param name="max"></param>
+            /// <returns></returns>
+            public static int RangeInt(int min, int max)
             {
                 return rng.Next(min, max + 1);
             }
 
-            public static float RangeFloat(float min, float max) // Returns a random float in range
+            /// <summary>
+            /// Returns a random float in range
+            /// </summary>
+            /// <param name="min"></param>
+            /// <param name="max"></param>
+            /// <returns></returns>
+            public static float RangeFloat(float min, float max)
             {
                 return (float)rng.NextDouble() * (max - min) + min;
             }
 
-            public static bool RandBool() // Returns a random bool, true or false
+            /// <summary>
+            /// Returns a random bool, true or false
+            /// </summary>
+            /// <returns></returns>
+            public static bool RandBool()
             {
                 if (rng.Next(0, 2) == 1)
                     return true;
@@ -27,30 +46,62 @@ namespace Consyl_Engine.EngineContents
                 return false;
             }
 
-            public static bool RandBoolByChance(float chance = 0.5f) // returns true randomly based on the decimal chance it would do it
+            /// <summary>
+            /// returns true randomly based on the decimal chance it would do it
+            /// </summary>
+            /// <param name="chance"></param>
+            /// <returns></returns>
+            public static bool RandBoolByChance(float chance = 0.5f)
             {
                 return chance >= RangeFloat(0.0f, 1.0f);
             }
 
-            public static int RandInt(int max) // Returns a random int between 0 to max
+            /// <summary>
+            /// Returns a random int between 0 to max
+            /// </summary>
+            /// <param name="max"></param>
+            /// <returns></returns>
+            public static int RandInt(int max)
             {
                 return rng.Next(max + 1);
             }
-        } // class that stores functions that returns rng-related values
+        }
 
+        /// <summary>
+        /// class that stores functions that are related to Vector2
+        /// </summary>
         public class Vec2D
         {
-            public static float Distance2D(Vector2 point1, Vector2 point2) // Calculates the 2D distance between two points/vectors
+            /// <summary>
+            /// Calculates the 2D distance between two points/vectors
+            /// </summary>
+            /// <param name="point1"></param>
+            /// <param name="point2"></param>
+            /// <returns></returns>
+            public static float Distance2D(Vector2 point1, Vector2 point2)
             {
                 return MathF.Sqrt(MathF.Pow(point2.X - point1.X, 2) + MathF.Pow(point2.Y - point1.Y, 2));
             }
 
-            public static Vector2 Midpoint2D(Vector2 point1, Vector2 point2) // Returns the center between two 2D vectors
+            /// <summary>
+            /// Returns the center between two 2D vectors
+            /// </summary>
+            /// <param name="point1"></param>
+            /// <param name="point2"></param>
+            /// <returns></returns>
+            public static Vector2 Midpoint2D(Vector2 point1, Vector2 point2)
             {
                 return (point1 + point2) / 2;
             }
 
-            public static Vector2 RotateAroundPoint(Vector2 pointToRotate, Vector2 centerPoint, float degreesAngle) // Returns the position of a point after being rotated by a given degrees around a given point
+            /// <summary>
+            /// Returns the position of a point after being rotated by a given degrees around a given point
+            /// </summary>
+            /// <param name="pointToRotate"></param>
+            /// <param name="centerPoint"></param>
+            /// <param name="degreesAngle"></param>
+            /// <returns></returns>
+            public static Vector2 RotateAroundPoint(Vector2 pointToRotate, Vector2 centerPoint, float degreesAngle)
             {   
                 float radianAngle = Numbers.DegreeToRad(degreesAngle); // Converts Degrees angle to Radian angle
                 float cosTheta = MathF.Cos(radianAngle);
@@ -58,34 +109,69 @@ namespace Consyl_Engine.EngineContents
 
                 return new Vector2((cosTheta * (pointToRotate.X - centerPoint.X) - sinTheta * (pointToRotate.Y - centerPoint.Y) + centerPoint.X), (sinTheta * (pointToRotate.X - centerPoint.X) + cosTheta * (pointToRotate.Y - centerPoint.Y) + centerPoint.Y));
             }
-        } // class that stores functions that are related to Vector2
+        }
 
+        /// <summary>
+        /// class that stores functions that are related to Vector3
+        /// </summary>
         public class Vec3D
         {
-            public static float Distance3D(Vector3 point1, Vector3 point2) // Calculates the 3D distance between two points/vectors
+            /// <summary>
+            /// Calculates the 3D distance between two points/vectors
+            /// </summary>
+            /// <param name="point1"></param>
+            /// <param name="point2"></param>
+            /// <returns></returns>
+            public static float Distance3D(Vector3 point1, Vector3 point2)
             {
                 return MathF.Sqrt(MathF.Pow(point2.X - point1.X, 2) + MathF.Pow(point2.Y - point1.Y, 2) + MathF.Pow(point2.Z - point1.Z, 2));
             }
 
-            public static Vector3 Midpoint3D(Vector3 point1, Vector3 point2) // Returns the center between two 3D vectors
+            /// <summary>
+            /// Returns the center between two 3D vectors
+            /// </summary>
+            /// <param name="point1"></param>
+            /// <param name="point2"></param>
+            /// <returns></returns>
+            public static Vector3 Midpoint3D(Vector3 point1, Vector3 point2)
             {
                 return (point1 + point2) / 2;
             }
 
-            public static Vector2 Vec3DToVec2D(Vector3 vec3D, float depth = 100) // Converts 3D Vectors into 2D vectors (Can be used to render simple 3D graphics)
+            /// <summary>
+            /// Converts 3D Vectors into 2D vectors (Can be used to render simple 3D graphics)
+            /// </summary>
+            /// <param name="vec3D"></param>
+            /// <param name="depth"></param>
+            /// <returns></returns>
+            public static Vector2 Vec3DToVec2D(Vector3 vec3D, float depth = 100)
             {
                 return new Vector2((vec3D.X * (depth / vec3D.Z)) + (gfx.drawWidth / 2), (vec3D.Y * (depth / vec3D.Z)) + (gfx.drawHeight / 2));
             }
-        } // class that stores functions that are related to Vector3
+        }
 
+        /// <summary>
+        /// class that stores functions that are related to numbers
+        /// </summary>
         public class Numbers
         {
-            public static float Distance1D(float num1, float num2) // calculates the distance between two single numbers
+            /// <summary>
+            /// calculates the distance between two single numbers
+            /// </summary>
+            /// <param name="num1"></param>
+            /// <param name="num2"></param>
+            /// <returns></returns>
+            public static float Distance1D(float num1, float num2)
             {
                 return MathF.Sqrt(MathF.Pow(num1 - num2, 2));
             }
 
-            public static float AverageNum(float[] floatArray) // Will return the average of numbers inside an array
+            /// <summary>
+            /// Will return the average of numbers inside an array
+            /// </summary>
+            /// <param name="floatArray"></param>
+            /// <returns></returns>
+            public static float AverageNum(float[] floatArray)
             {
                 float value = 0.0f;
                 for (int i = 0; i < floatArray.Length; i++)
@@ -94,15 +180,25 @@ namespace Consyl_Engine.EngineContents
                 return value / floatArray.Length;
             }
 
-            public static float DegreeToRad(float degree) // Converts degrees to radians
+            /// <summary>
+            /// Converts degrees to radians
+            /// </summary>
+            /// <param name="degree"></param>
+            /// <returns></returns>
+            public static float DegreeToRad(float degree)
             {
                 return (degree * MathF.PI) / 180.0f;
             }
 
-            public static float RadToDegree(float radian) // Converts radian to degrees
+            /// <summary>
+            /// Converts radian to degrees
+            /// </summary>
+            /// <param name="radian"></param>
+            /// <returns></returns>
+            public static float RadToDegree(float radian)
             {
                 return (radian * 180.0f) / MathF.PI;
             }
-        } // class that stores functions that are related to numbers
+        }
     }
 }
